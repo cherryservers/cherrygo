df5e0827964f7e3391a9a7fc2935c291c497bf4c
Arturas Razinskij <arturas@Arturass-MacBook-Pro.local>
2019-03-08 13:41:33 +0200

    sshkeys tests added

--- sshkey_test.go
+++ sshkey_test.go
@@ -38,6 +38,63 @@ func createSSHKey(t *testing.T, c *Client) SSHKeys {
 
 	return sshkey
 }
+
+func TestSSHKeyCreate(t *testing.T) {
+
+	t.Parallel()
+
+	c := setupClient(t)
+
+	sshCreateRequest := CreateSSHKey{
+		Label: "CHERRY_TEST_LABEL-" + RandStringBytes(8),
+		Key:   makeRandKey(t),
+	}
+
+	sshkey, _, err := c.SSHKey.Create(&sshCreateRequest)
+	if err != nil {
+		t.Fatalf("error while creating ssh key: %v", err)
+	}
+
+	defer c.SSHKey.Delete(&DeleteSSHKey{ID: (strconv.Itoa(sshkey.ID))})
+
+	if sshkey.Label != sshCreateRequest.Label {
+		t.Fatalf("ssh keys label doesn't match, expected: %v, current: %v", sshCreateRequest.Label, sshkey.Label)
+	}
+
+	if sshkey.Key != sshCreateRequest.Key {
+		t.Fatalf("ssh keys doesn'n match, expected: %v, current: %v", sshCreateRequest.Key, sshkey.Key)
+	}
+
+}
+
+func TestSSHKeyUpdate(t *testing.T) {
+
+	t.Parallel()
+
+	c := setupClient(t)
+
+	sshKey := createSSHKey(t, c)
+
+	newLabel := sshKey.Label + "-new"
+
+	sshUpateRequest := UpdateSSHKey{
+		Label: newLabel,
+	}
+
+	sshKeyString := strconv.Itoa(sshKey.ID)
+
+	key, _, err := c.SSHKey.Update(sshKeyString, &sshUpateRequest)
+	if err != nil {
+		t.Fatalf("error while updating ssh key: %v", err)
+	}
+
+	defer c.SSHKey.Delete(&DeleteSSHKey{ID: sshKeyString})
+
+	if key.Label != sshUpateRequest.Label {
+		t.Fatalf("expected label: %v, found: %v", sshUpateRequest.Label, key.Label)
+	}
+
+}
 func TestSSHKeyDelete(t *testing.T) {
 
 	t.Parallel()
@@ -56,4 +113,60 @@ func TestSSHKeyDelete(t *testing.T) {
 	if err == nil {
 		t.Fatalf("it seems key wasn't deleted: %v", sshkey)
 	}
+}
+
+func TestSSHKeyList(t *testing.T) {
+
+	t.Parallel()
+
+	c := setupClient(t)
+
+	sshKey := createSSHKey(t, c)
+
+	sshKeyString := strconv.Itoa(sshKey.ID)
+
+	defer c.SSHKey.Delete(&DeleteSSHKey{ID: sshKeyString})
+
+	sshkey, _, err := c.SSHKey.List(sshKeyString)
+	if err != nil {
+		t.Fatalf("unable to get SSH key: %v", err)
+	}
+
+	if sshKey.ID != sshkey.ID {
+		t.Fatalf("keys IDs doesn't match, expected: %v, current: %v", sshKey.ID, sshkey.ID)
+	}
+
+	if sshKey.Fingerprint != sshkey.Fingerprint {
+		t.Fatalf("keys fingerprints doesn't match, expected: %v, current: %v", sshKey.Fingerprint, sshkey.Fingerprint)
+	}
+
+	if sshKey.Label != sshkey.Label {
+		t.Fatalf("keys labels doesn't match, expected: %v, current: %v", sshKey.Label, sshkey.Label)
+	}
+}
+
+func TestSSHKeysList(t *testing.T) {
+
+	t.Parallel()
+
+	c := setupClient(t)
+
+	sshKey := createSSHKey(t, c)
+
+	sshKeyString := strconv.Itoa(sshKey.ID)
+
+	defer c.SSHKey.Delete(&DeleteSSHKey{ID: sshKeyString})
+
+	sshkeys, _, err := c.SSHKeys.List()
+	if err != nil {
+		t.Fatalf("unable to get SSH key: %v", err)
+	}
+
+	for _, v := range sshkeys {
+		if v.ID == sshKey.ID {
+			return
+		}
+	}
+	t.Fatalf("unable to find created key in a list: %v", sshKey.ID)
+
 }
